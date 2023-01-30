007f2bae42d791356661dbda3ae065f3e040809d
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-01-30 16:01:35 +0530

    Set up react project with CRA

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' component="" />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
